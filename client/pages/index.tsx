081f7ebf320ae169a1114e1d12db06392117d411
apolkingg8 <apolkingg8@gmail.com>
2021-06-19 15:09:18 +0800

    :tada: Amingo! coding challenge

---
+++ client/pages/index.tsx
@@ -0,0 +1,24 @@
+import React, {FC} from "react";
+import {observer} from "mobx-react";
+import {computedFn} from "mobx-utils";
+import {stylesheet} from "typestyle";
+
+export interface indexProps {
+
+}
+
+let getStyles = computedFn(() => (stylesheet({
+    wrap: {},
+})))
+
+let index: FC<indexProps> = (props) => {
+    let styles = getStyles()
+
+    return (
+        <div className={styles.wrap}>
+            Amingo!
+        </div>
+    )
+}
+
+export default observer(index)
