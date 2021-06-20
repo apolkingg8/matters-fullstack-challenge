aca50065017b04d60562944517f1cf722bebc9a0
apolkingg8 <apolkingg8@gmail.com>
2021-06-20 17:08:54 +0800

    :art: article index redirect

--- client/pages/article/index.tsx
+++ client/pages/article/index.tsx
@@ -1,11 +1,7 @@
-import React, {FC, useEffect, useState} from "react";
+import React, {FC, useEffect} from "react";
 import {observer} from "mobx-react";
 import {computedFn} from "mobx-utils";
 import {stylesheet} from "typestyle";
-import articleService from "../../service/articleService";
-import IArticle from "../../../common/IArticle";
-import themeStore from "../../store/themeStore";
-import styleStore from "../../store/styleStore";
 
 export interface ArticlesPageProps {
 
@@ -13,62 +9,18 @@ export interface ArticlesPageProps {
 
 let getStyles = computedFn(() => (stylesheet({
     wrap: {
-        ...styleStore.full,
-        ...styleStore.centerCol,
-    },
-    card: {
-        width: 480,
-        margin: 16,
-        padding: 16,
-        background: themeStore.theme.palette.background.paper,
-        borderRadius: 8,
-    },
-    title: {
-        ...themeStore.theme.typography.h4,
-        marginBottom: 8,
-    },
-    content: {
-        ...themeStore.theme.typography.body1,
+
     },
 })))
 
 let ArticlesPage: FC<ArticlesPageProps> = (props) => {
-    let [articles, setArticles] = useState<IArticle[]>([])
-    let [total, setTotal] = useState<number>(0)
     let styles = getStyles()
 
     useEffect(()=> {
-        let getArticles = async ()=> {
-            let articles = await articleService.getAll()
-            let total = await articleService.getTotalCount()
-            setArticles(articles)
-        }
-
-        getArticles()
+        location.href = "/article/p/1"
     }, [])
 
-    return (
-        <div className={styles.wrap}>
-            <a href={`/article/new`}>
-                Add article
-            </a>
-            {articles.map((article)=> {
-                return (
-                    <a
-                        className={styles.card}
-                        href={`/article/${article.id}`}
-                    >
-                        <div className={styles.title}>
-                            {article.title}
-                        </div>
-                        <div className={styles.content}>
-                            {article.content}
-                        </div>
-                    </a>
-                )
-            })}
-        </div>
-    )
+    return null
 }
 
 export default observer(ArticlesPage)
